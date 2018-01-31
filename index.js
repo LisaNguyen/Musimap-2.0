771c632d6fccab971fd1edc64064402edb7beed1
Lisa <hello@lisanguyen.me>
2018-01-31 23:21:36 +0000

    passport setup

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const express = require('express');
+const passport = require('passport');
+const { spotifyAuthClientId, spotifyAuthClientSecret  } = require('./config/config');
+const SpotifyStrategy = require('passport-spotify').Strategy;
+
 const app = express();
+passport.use(new SpotifyStrategy({
+  clientID: spotifyAuthClientId,
+  clientSecret: spotifyAuthClientSecret,
+  callbackURL: 'http://localhost:8080/callback'
+}, (accessToken, refreshToken, profile, done) => {
+  console.log(profile);
+  done(null, profile);
+}));
 
 const PORT = process.env.PORT || 8080;
 
