206c3ae1896d78de9bf3902fe3b9b46d4aee6b81
Lisa <hello@lisanguyen.me>
2018-02-14 23:39:25 +0000

    add short overview and styling to navlinks

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -12,6 +12,7 @@ export default class Header extends Component {
             <div className="col-xs-4 mr-auto opening-line">
               <p className="secondary-line">We Are Live</p>
               <h3>Discover the beats of <br />the world.</h3>
+              <h5>Musimap is an interactive tool that allows you to explore the different, distinct sounds of over 800 cities.</h5>
               <Button id="sign-up">Sign Up</Button>{' '}
               <Button id="sign-in"  outline color="secondary">Log In</Button>{' '}
             </div>
