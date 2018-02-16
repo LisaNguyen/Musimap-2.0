097a4efbb2a53e75a9490314c37e8ef4863006a0
Lisa <hello@lisanguyen.me>
2018-02-16 23:48:18 +0000

    react router setup

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Header from './Header';
 
 const App = () => {
   return (
     <div>
       <Header />
+      <BrowserRouter>
+        
+      </BrowserRouter>
     </div>
   );
 };
