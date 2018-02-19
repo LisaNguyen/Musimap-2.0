7cf09da96f80d320f0d178ca10a3a547f07c0b9e
Lisa <hello@lisanguyen.me>
2018-02-19 23:25:47 +0000

    handle case where action type is fetch user

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -4,6 +4,6 @@ import { FETCH_USER }  from './types';
 export const fetchUser = () => {
   return function(dispatch) {
     axios.get('/api/current_user')
-    .then(res => dispatch({type: FETCH_USER, payload: res}));
+    .then(res => dispatch({type: FETCH_USER, payload: res.data}));
   };
 };
